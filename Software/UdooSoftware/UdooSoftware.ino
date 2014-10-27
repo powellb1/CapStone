373479bae17c6ad991c23009ba7ea636cb55c618
asenop <asenopoulos@gmail.com>
2014-10-26 20:22:47 -0400

    Linefollow code
    Code updated with PID library and average of QREs.

--- Software/UdooSoftware/UdooSoftware.ino
+++ Software/UdooSoftware/UdooSoftware.ino
@@ -1,16 +1,32 @@
 
-//Define pins for QRE1113s
-int QRE1Pin = 2;
-int QRE2Pin = 3;
-int QRE3Pin = 4;
-int QRE4Pin = 5;
-int QRE5Pin = 6;
+#include <PID_v1.h>
 
+//Define pins for QRE1113s
+int QRE1Pin = 5;
+int QRE2Pin = 6;
+int QRE3Pin = 7;
+int QRE4Pin = 8;
+int QRE5Pin = 9;
+
+//Pins for Motors
+int dcLeft = 3;
+int dcRight = 4;
+
+//For PID
+double pidSetpoint, pidInput, pidOutput;
+PID pid = (&pidInput, &pidOutput, &pidSetpoint, 2,5,1, DIRECT);
 
 
 void setup() {
   Serial.begin(9600);
-
+  pinMode(dcLeft, OUTPUT);
+  pinMode(dcRight, OUTPUT);
+  
+  //PID setup
+  pidInput = 3000;
+  pidSetpoint = 100;
+  pid.SetMode(AUTOMATIC);
+  
 }
 
 void loop() {
@@ -22,8 +38,13 @@ void loop() {
   int QRE4val = readQRE(QRE4Pin);
   int QRE5val = readQRE(QRE5Pin);
   
+  //Average values for PID
+ 
+   pidInput = (QRE1val*-2 + QRE2val*-1 + QREval3*0 + QREval4*1 + QREval5*2);
+   pid.Compute();
+   
   //Adjust motors based off of values from QREs
-  Serial.println("Readings retreived");
+  
   
 }
 
